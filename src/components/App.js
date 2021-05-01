a5bdcf7333782128c5f11206dcafe29ad94dc412
oscarporras02 <oscarrussi@outlook.com>
2021-04-30 20:17:43 -0500

    add options component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Options from './Options'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Options/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Options.js
@@ -0,0 +1,11 @@
+function Options() {
+  return (
+    <div className="options">
+      <button>Play</button>
+      <button>Settings</button>
+      <button>Feedback</button>
+    </div>
+  );
+}
+
+export default Options;
