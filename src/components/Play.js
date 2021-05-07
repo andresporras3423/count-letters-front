fb025bed8b8305364abcb7f4272dd9b19df6ee8d
oscarporras02 <oscarrussi@outlook.com>
2021-05-06 22:26:20 -0500

    using timeout to reload the component

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,104 +1,28 @@
-import {useEffect} from 'react';
-import useState from 'react-usestateref'
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import useState from 'react-usestateref';
+import PlayContent from './PlayContent';
 
-function Play(props) {
-  const {whitespaces, total_letters, questions} = props;
-  const letters_x = Math.round((2*total_letters)**0.5);
-  const [initialTime, setInitialTime] = useState(new Date());
-  const [totalTime, setTotalTime] = useState(0);
-  const [word, setWord] = useState('');
-  const [sol, setSol, refSol] = useState(0);
-  const [answer, setAnswer] = useState('');
-  const [counterQuestions, setCounterQuestions] = useState(0);
-  const [corrects, setCorrects, refCorrects] = useState(0);
-  const [char, setChar] = useState('');
-  const [messageAnswer, setMessageAnswer] = useState('');
+function Play() {
+  const [showContent, setShowContent, refShowContent] = useState(false);
 
-  const updateCurrentTime = ()=>{
-    const new_value = Math.round((new Date() - initialTime.valueOf())/1000);
-    setTotalTime(new_value);
-    setTimeout(updateCurrentTime, 1000);
+  const forceUpdate = ()=>{
+    setShowContent(true);
+    setTimeout(reloadAgain, 10);
+  };
+
+  const reloadAgain = ()=>{
+    setShowContent(false);
+  };
+
+  const showPlayContent = ()=>{
+    if(!refShowContent.current) return <PlayContent forceUpdate={forceUpdate} />
+    else return <></>;
   };
   
-  const question = ()=>{
-    setCounterQuestions(counterQuestions+1);
-    let alphabet= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    let subAlphabet =[];
-    let nWord='';
-    [...Array(5).keys()].forEach(()=>{
-      let numb = Math.round(Math.random()*(alphabet.length-1));
-      subAlphabet.push(alphabet[numb]);
-      alphabet.splice(numb,1);
-    });
-    setChar(subAlphabet[0]);
-    [...Array(total_letters).keys()].forEach((j)=>{
-      if((j+1)%letters_x==1) nWord+="<span>";
-      let numb = Math.round(Math.random()*4);
-      if(numb===0) setSol(refSol.current+1);
-      nWord+=subAlphabet[numb];
-      for(let k=0;k<whitespaces;k++) nWord+=" ";
-      if((j+1)%letters_x==0){
-        nWord+="</span>";
-        for(let k=0;k<whitespaces;k++) nWord+="<br/>";
-      }
-      setWord(nWord);
-    });
-  }
-
-  const nextQuestion = ()=>{
-    console.log(`answer: ${answer}, sol: ${sol}`)
-    if(answer===""+sol){
-      setCorrects(refCorrects.current+1);
-      setMessageAnswer("correct answer");
-    }
-    else{
-      setMessageAnswer(`Incorrect, the solution was ${sol}`);
-    }
-    setSol(0);
-    setAnswer('');
-    question();
-  };
-
-  useEffect(()=>{
-    question();
-    updateCurrentTime();
-  }, [])
     return (
       <div>
-        <h4>Total questions: {questions}, whitespaces: {whitespaces}, letters: {total_letters}</h4>
-          <h4>Total time: {totalTime}</h4>
-          <h4>Score: {refCorrects.current}/{counterQuestions}</h4>
-          <h4>How many?: {char}</h4>
-          <div>
-            <pre dangerouslySetInnerHTML={{ __html: word }}></pre>
-          </div>
-          <div>
-            <input type="text" value={answer} onChange={(el)=>setAnswer(el.target.value)}></input>
-            <button onClick={nextQuestion}>submit</button>
-          </div>
-          <div>{messageAnswer}</div>
+        {showPlayContent()}
       </div>
     );
   }
-
-  const mapStateToProps = state => ({
-    whitespaces: state.config.whitespaces,
-    total_letters: state.config.total_letters,
-    questions: state.config.questions,
-    });
   
-  Play.propTypes = {
-    whitespaces: PropTypes.number,
-    total_letters: PropTypes.number,
-    questions: PropTypes.number,
-  };
-  
-  Play.defaultProps = {
-    whitespaces: 0,
-    total_letters: 0,
-    questions: 0,
-  };
-  
-  export default connect(mapStateToProps)(Play);
+  export default Play;
---
+++ src/components/PlayContent.js
@@ -0,0 +1,134 @@
+import {useEffect} from 'react';
+import useState from 'react-usestateref';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function PlayContent(props) {
+  const {whitespaces, total_letters, questions, forceUpdate} = props;
+  const letters_x = Math.round((2*total_letters)**0.5);
+  const [initialTime, setInitialTime] = useState(new Date());
+  const [totalTime, setTotalTime] = useState(0);
+  const [word, setWord] = useState('');
+  const [sol, setSol, refSol] = useState(0);
+  const [answer, setAnswer] = useState('');
+  const [counterQuestions, setCounterQuestions] = useState(0);
+  const [corrects, setCorrects, refCorrects] = useState(0);
+  const [char, setChar] = useState('');
+  const [messageAnswer, setMessageAnswer] = useState('');
+  const [playingGame, setPlayingGame, refPlayingGame] = useState(1);
+
+  const updateCurrentTime = ()=>{
+    if(refPlayingGame.current!==1){
+      return;
+    }
+    //console.log(playingGame);
+    const new_value = Math.round((new Date() - initialTime.valueOf())/1000);
+    setTotalTime(new_value);
+    setTimeout(updateCurrentTime, 1000);
+  };
+  
+  const question = ()=>{
+    setCounterQuestions(counterQuestions+1);
+    let alphabet= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let subAlphabet =[];
+    let nWord='';
+    [...Array(5).keys()].forEach(()=>{
+      let numb = Math.round(Math.random()*(alphabet.length-1));
+      subAlphabet.push(alphabet[numb]);
+      alphabet.splice(numb,1);
+    });
+    setChar(subAlphabet[0]);
+    [...Array(total_letters).keys()].forEach((j)=>{
+      if((j+1)%letters_x==1) nWord+="<span>";
+      let numb = Math.round(Math.random()*4);
+      if(numb===0) setSol(refSol.current+1);
+      nWord+=subAlphabet[numb];
+      for(let k=0;k<whitespaces;k++) nWord+=" ";
+      if((j+1)%letters_x==0){
+        nWord+="</span>";
+        for(let k=0;k<whitespaces;k++) nWord+="<br/>";
+      }
+      setWord(nWord);
+    });
+  }
+
+  const nextQuestion = ()=>{
+    //console.log(`answer: ${answer}, sol: ${sol}`);
+    if(answer===""+sol){
+      setCorrects(refCorrects.current+1);
+      setMessageAnswer("correct answer");
+    }
+    else{
+      setMessageAnswer(`Incorrect, the solution was ${sol}`);
+    }
+    if(counterQuestions===questions){
+      setPlayingGame(0);
+      return;
+    }
+    setSol(0);
+    setAnswer('');
+    question();
+  };
+
+  useEffect(()=>{
+    question();
+    updateCurrentTime();
+  }, []);
+
+  const displayContent = ()=>{
+    if(refPlayingGame.current===1){
+      return (
+        <>
+        <h4>Total questions: {questions}, whitespaces: {whitespaces}, letters: {total_letters}</h4>
+          <h4>Total time: {totalTime}</h4>
+          <h4>total corrects: {refCorrects.current}/{counterQuestions}</h4>
+          <h4>How many?: {char}</h4>
+          <div>
+            <pre dangerouslySetInnerHTML={{ __html: word }}></pre>
+          </div>
+          <div>
+            <input type="text" value={answer} onChange={(el)=>setAnswer(el.target.value)}></input>
+            <button onClick={nextQuestion}>submit</button>
+          </div>
+          <div>{messageAnswer}</div>
+        </>
+      )
+    }
+    return (
+      <>
+      <h4>Final time: {totalTime}</h4>
+      <h4>Final corrects: {refCorrects.current}/{counterQuestions}</h4>
+      <h4>Final score: {totalTime*(2**(questions-refCorrects.current))}</h4>
+      <button onClick={forceUpdate}>Play again</button>
+      </>
+    );
+  };
+
+    return (
+      <div>
+        {displayContent()}
+      </div>
+    );
+  }
+
+  const mapStateToProps = state => ({
+    whitespaces: state.config.whitespaces,
+    total_letters: state.config.total_letters,
+    questions: state.config.questions,
+    });
+  
+    PlayContent.propTypes = {
+    whitespaces: PropTypes.number,
+    total_letters: PropTypes.number,
+    questions: PropTypes.number,
+    forceUpdate: PropTypes.func,
+  };
+  
+  PlayContent.defaultProps = {
+    whitespaces: 0,
+    total_letters: 0,
+    questions: 0,
+    forceUpdate: null,
+  };
+  
+  export default connect(mapStateToProps)(PlayContent);
