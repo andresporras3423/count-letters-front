5a46aba35c14a3cf16598f772dc367bb2d8b68ad
oscarporras02 <oscarrussi@outlook.com>
2021-05-09 09:24:53 -0500

    update README file

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -2,7 +2,7 @@ function Instructions() {
    return (
      <div className='margin-div'>
        <h5>Welcome to my game</h5>
-       <p>count letter is self descriptive, count how many of the specified letter you can see in the matrix.</p>
+       <p>count letters game is self descriptive, count how many of the specified letter you can see in the matrix.</p>
        <p>In settings you can update whitespaces which is the space between rows and between columns. You can also modify the number of letters in the matrix and how many questions.</p>
        <p>In feedback you can see the global stats of the game. The score stats are filtered using the current settings (number of questions and letters). On the other hand, the stats for questions are not filtered.</p>
        <p>The 'Play' tab is... to play the game. That's it, hope you like it.</p>
