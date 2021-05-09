503b7cf43dc148859ef452dc339ff36ce40fc93c
oscarporras02 <oscarrussi@outlook.com>
2021-05-09 08:52:11 -0500

    putthe after answer  message before the form

--- src/components/PlayContent.js
+++ src/components/PlayContent.js
@@ -99,11 +99,11 @@ function PlayContent(props) {
           <div>
             <pre dangerouslySetInnerHTML={{ __html: word }}></pre>
           </div>
+          <div className="messageHeight">{messageAnswer}</div>
           <div className="form-group play-form">
             <input className='form-control' onKeyPress={(e)=>{if (e.charCode === 13) nextQuestion()}} ref={inputRef} type="number" value={answer} onChange={(el)=>setAnswer(el.target.value)}></input>
             <button className='btn btn-dark' onClick={nextQuestion}>submit</button>
           </div>
-          <div>{messageAnswer}</div>
         </>
       )
     }
